244f76a9f13b08b940ca01674f93325a3da0282e
Pieter Schreurs <pieter_j_schreurs@hotmail.com>
2018-09-11 09:12:19 +0200

    Prefabs imported and in glob.

--- Assets/Scripts/Glob.cs
+++ Assets/Scripts/Glob.cs
@@ -19,16 +19,37 @@ public static class Glob {
     public const string tilePrefab = "Tile";
 
     public const int buildingCount = 2;
+    public const int factoriesVariations = 3;
 
     public const string housePrefab = "House";
-    public const string factoryPrefab = "Factory";
+    public const string factoryPrefab0 = "Factory0";
+    public const string factoryPrefab1 = "Factory1";
+    public const string factoryPrefab2 = "Factory2";
+    public const string factoryPrefab3 = "Factory3";
+    public const string factoryPrefab4 = "Factory4";
     public const string parkPrefab = "Park";
+
     public static Building[] GetBuildingPrefabs()
     {
         Building[] buildings = new Building[buildingCount];
         buildings[0] = Resources.Load<House>(housePrefab);
-        buildings[1] = Resources.Load<Factory>(factoryPrefab);
+        buildings[1] = Resources.Load<Factory>(factoryPrefab0);
+        //buildings[2] = Resources.Load<Factory>(factoryPrefab1);
+        //buildings[3] = Resources.Load<Factory>(factoryPrefab2);
+        //buildings[4] = Resources.Load<Factory>(factoryPrefab3);
+        //buildings[5] = Resources.Load<Factory>(factoryPrefab4);
         return buildings;
+    }
+
+    public static Factory[] GetFactories()
+    {
+        Factory[] factories = new Factory[factoriesVariations];
+        factories[0] = Resources.Load<Factory>(factoryPrefab0);
+        factories[1] = Resources.Load<Factory>(factoryPrefab1);
+        factories[2] = Resources.Load<Factory>(factoryPrefab2);
+        factories[3] = Resources.Load<Factory>(factoryPrefab3);
+        factories[4] = Resources.Load<Factory>(factoryPrefab4);
+        return factories;
     }
 
     public const string buildingImagePrefab = "BuildingIcon";
